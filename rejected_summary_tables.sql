ab994a46a78c5f752700730851e8515b4f40e81f
xkayser1997 <xkayser1997@gmail.com>
2026-07-26 22:47:29 -0500

    Create tables for rejection analysis by various criteria
    Added multiple tables to analyze rejections by loan amount, DTI, employment length, and loan purpose.

--- rejected_summary_tables.sql
+++ rejected_summary_tables.sql
@@ -7,3 +7,48 @@ SELECT
   ROUND(AVG(`dti_clean`),4) as avg_dti,
   ROUND(AVG(`Risk_Score`),2) as avg_risk_score,
 FROM `cedar-turbine-501913-v0.lend_club.rejected_clean`
+
+--Rejections by Loan amount bands
+CREATE OR REPLACE TABLE `cedar-turbine-501913-v0.lend_club.rejected_loan_amnt`
+AS
+SELECT 
+  requested_amnt_band,
+  COUNT(*) as total_rejected_apps
+
+FROM `cedar-turbine-501913-v0.lend_club.rejected_clean`
+GROUP BY requested_amnt_band 
+ORDER BY total_rejected_apps DESC
+
+--Rejections by DTI bands
+CREATE OR REPLACE TABLE `cedar-turbine-501913-v0.lend_club.rejected_dti_band`
+AS
+SELECT 
+  dti_band,
+  COUNT(*) as total_rejected_apps
+
+FROM `cedar-turbine-501913-v0.lend_club.rejected_clean`
+GROUP BY dti_band 
+ORDER BY dti_band;
+
+--Rejection by Employment Length
+CREATE OR REPLACE TABLE `cedar-turbine-501913-v0.lend_club.rejected_employment`
+AS
+SELECT 
+  `Employment Length`,
+  COUNT(*) as total_rejected_apps
+
+FROM `cedar-turbine-501913-v0.lend_club.rejected_clean`
+GROUP BY `Employment Length` 
+ORDER BY `Employment Length`;
+
+--Rejections by loan purpose
+CREATE OR REPLACE TABLE `cedar-turbine-501913-v0.lend_club.rejected_purpose`
+AS
+SELECT 
+  `Loan Purpose`,
+  COUNT(*) as total_rejected_apps
+
+FROM `cedar-turbine-501913-v0.lend_club.rejected_clean`
+GROUP BY `Loan Purpose` 
+ORDER BY `Loan Purpose`;
+
